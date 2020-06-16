85441a5f0fd2fb543726da92fbb6927913bb5805
89hardy <himanshuawasthi9@gmail.com>
2020-06-16 15:35:24 +1000

    JS basics, connection, and first exercise

---
+++ Javascripts/Part3_Exercise_Solution.js
@@ -0,0 +1,4 @@
+var lbs = prompt("Enter the weight in pounds(lbs):");
+var kgs = lbs *0.454
+alert("Weight in kilogram is: "+kgs);
+console.log("Conversion Completed");
---
+++ Javascripts/myscript.js
@@ -0,0 +1,5 @@
+alert("Welcome to your bank")
+
+var deposit = prompt("How much would you like to deposit today? ")
+alert("You deposited: "+deposit)
+console.log("You are cool")
