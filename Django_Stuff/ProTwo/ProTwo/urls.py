a58d8c7669ba828375950148a98b0c372c59658d
Himanshu Awasthi <himanshuawasthi9@gmail.com>
2020-07-09 22:28:22 +1000

    Django Part 1

--- Django_Stuff/ProTwo/ProTwo/urls.py
+++ Django_Stuff/ProTwo/ProTwo/urls.py
@@ -1,4 +1,4 @@
-"""first_django_project URL Configuration
+"""ProTwo URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -13,14 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
-from first_app import views
-from django.conf.urls import include
+from django.conf.urls import url
+from AppTwo import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'first_app/', include('first_app.urls')),
     path('admin/', admin.site.urls),
 ]
