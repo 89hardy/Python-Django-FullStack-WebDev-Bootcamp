06728ff5f6713bfb1649afc5770d63be6c84c671
Himanshu Awasthi <himanshuawasthi9@gmail.com>
2020-07-20 15:28:31 +1000

    Django mod 1 complete

--- Django_Stuff/ProTwo/AppTwo/views.py
+++ Django_Stuff/ProTwo/AppTwo/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<em>My Second App</em>")
+    # return HttpResponse("<em>My Second App</em>")
+    my_dict = {'insert_me':"I'm coming from ProTwo/help.html"}
+    return render(request, 'ProTwo/help.html',context=my_dict)
--- Django_Stuff/first_django_project/first_app/views.py
+++ Django_Stuff/first_django_project/first_app/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World!")
+    # return HttpResponse("Hello World!")
+    my_dict = {'insert_me':"Hello! I'm coming from first_app/index.html"}
+    return render(request, 'first_app/index.html', context=my_dict)
